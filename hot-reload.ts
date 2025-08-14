6ef9458a1351c5d6972446c8ee97f01bff3a9555
Michael Naumov <mnaoumov@gmail.com>
2025-08-13 22:25:36 -0600

    Lint

--- hot-reload.ts
+++ hot-reload.ts
@@ -1,242 +1,337 @@
-import { Plugin, Notice, debounce, Platform, requireApiVersion, App, Component, FileSystemAdapter } from "obsidian";
-import { around } from "monkey-around"
+import type {
+    DataAdapter,
+    PluginSettingTab,
+    Stat
+} from 'obsidian';
+
+import { around } from 'monkey-around';
+import {
+    App,
+    Component,
+    debounce,
+    FileSystemAdapter,
+    Notice,
+    Platform,
+    Plugin,
+    requireApiVersion
+} from 'obsidian';
 
 const watchNeeded = !Platform.isMobile && !Platform.isMacOS && !Platform.isWin;
-
-export default class HotReload extends Plugin {
-
-    statCache = new Map();  // path -> Stat
-    run = taskQueue()
-
-    reindexPlugins = debounce(() => this.run(() => this.getPluginNames()), 250, true);
-
-    pluginReloaders: Record<string, ()=> unknown> = {}
-    pluginNames: Record<string, string> = {}
-    enabledPlugins = new Set<string>()
-    currentlyLoading = 0
-
-    settingReloader = new SettingReloader(this)
-
-    onload() {
-        this.app.workspace.onLayoutReady(async () => {
-            await this.getPluginNames();
-            this.addCommand({
-                id: "scan-for-changes",
-                name: "Check plugins for changes and reload them",
-                callback: this.reindexPlugins
-            })
-            this.registerEvent( this.app.vault.on("raw", this.onFileChange));
-            this.watch(this.app.plugins.getPluginFolder());
-        });
-    }
-
-    async watch(path: string) {
-        if (!(this.app.vault.adapter instanceof FileSystemAdapter)) {
-            return;
-        }
-
-        if (this.app.vault.adapter.watchers?.hasOwnProperty(path)) return;
-        if ((await this.app.vault.adapter.stat(path))?.type !== "folder") return;
-        if (watchNeeded || this.isSymlink(path)) this.app.vault.adapter.startWatchPath(path);
-    }
-
-    isSymlink = (() => {
-        try {
-            const {lstatSync} = require('fs');
-            return (path: string) => {
-                const realPath = [this.app.vault.adapter.basePath, path].join("/");
-                const lstat = lstatSync(realPath, {throwIfNoEntry: false});
-                return lstat && lstat.isSymbolicLink();
-            }
-        } catch (e) {
-            return () => true;
-        }
-    })();
-
-    checkVersions() {
-        return Promise.all(Object.values(this.pluginNames).map(this.checkVersion))
-    }
-
-    checkVersion = async (plugin: string) => {
-        const {dir} = (this.app.plugins.manifests[plugin] || {})
-        if (dir) for (const file of ["main.js", "styles.css"]) {
-            const path = `${dir}/${file}`;
-            const stat = await this.app.vault.adapter.stat(path);
-            if (stat) {
-                if (this.statCache.has(path) && stat.mtime !== this.statCache.get(path).mtime) {
-                    this.requestReload(plugin);
-                }
-                this.statCache.set(path, stat);
-            }
-        } else {
-            // Deleted plugin, stop watching
-            delete this.pluginNames[plugin]
-        }
-    }
-
-    async getPluginNames() {
-        const plugins: Record<string, string> = {}, enabled = new Set<string>();
-        for (const {id, dir} of Object.values(this.app.plugins.manifests)) {
-            this.watch(dir);
-            plugins[dir.split("/").pop()] = id;
-            if (
-                await this.app.vault.exists(dir+"/.git") ||
-                await this.app.vault.exists(dir+"/.hotreload")
-            ) enabled.add(id);
-        }
-        this.pluginNames = plugins;
-        this.enabledPlugins = enabled;
-        await this.checkVersions()
-    }
-
-    onFileChange = (filename: string) => {
-        if (!filename.startsWith(this.app.plugins.getPluginFolder()+"/")) return;
-        const path = filename.split("/");
-        const base = path.pop(), dir = path.pop();
-        if (path.length === 1 && dir === "plugins") return this.watch(filename);
-        if (path.length != 2) return;
-        const plugin = dir && this.pluginNames[dir];
-        if (base === "manifest.json" || base === ".hotreload" || base === ".git" || !plugin) return this.reindexPlugins();
-        if (base !== "main.js" && base !== "styles.css") return;
-        this.checkVersion(plugin);
-    }
-
-    requestReload(plugin: string) {
-        if (!this.enabledPlugins.has(plugin)) return;
-        const reloader = this.pluginReloaders[plugin] || (
-            this.pluginReloaders[plugin] = debounce(() => this.run(() => this.reload(plugin).catch(console.error)), 750, true)
-        );
-        reloader();
-    }
-
-    async reload(plugin: string) {
-        const plugins = this.app.plugins;
-
-        // Don't reload disabled plugins
-        if (!plugins.enabledPlugins.has(plugin)) return;
-
-        this.settingReloader.onPluginDisable(plugin);
-
-        await plugins.disablePlugin(plugin);
-        console.debug("disabled", plugin);
-
-        // Ensure sourcemaps are loaded (Obsidian 0.14+)
-        const oldDebug = localStorage.getItem("debug-plugin");
-        localStorage.setItem("debug-plugin", "1");
-        const uninstall = preventSourcemapStripping(this.app, plugin)
-        try {
-            await plugins.enablePlugin(plugin);
-        } finally {
-            // Restore previous setting
-            if (oldDebug === null) localStorage.removeItem("debug-plugin"); else localStorage.setItem("debug-plugin", oldDebug);
-            uninstall?.()
-        }
-        console.debug("enabled", plugin);
-        new Notice(`Plugin "${plugin}" has been reloaded`);
-    }
-}
-
-function preventSourcemapStripping(app: App, pluginName: string) {
-    if (requireApiVersion("1.6")) return(around(app.vault.adapter, {
-        read(old) {
-            return function (path: string) {
-                const res = old.apply(this, arguments as any)
-                if (!path.endsWith(`/${pluginName}/main.js`)) return res
-                return res.then(txt => txt+'\n/* nosourcemap */')
-            }
-        },
-    }))
-}
-
-function taskQueue() {
-    let last: Promise<any> = Promise.resolve();
-    return <T>(action?: () => T|PromiseLike<T>): Promise<T> => {
-        return !action ? last : last = new Promise<T>(
-            (res, rej) => last.finally(
-                () => { try { res(action()); } catch(e) { rej(e); } }
-            )
-        )
-    }
-}
 
 /**
  * Reload the settings tab (and scroll position) of a setting tab
  */
 class SettingReloader extends Component {
-    constructor(public plugin: Plugin) { super(); }
+    private app: App;
 
-    app = this.plugin.app
-    left = 0
-    top = 0
-    lastTab: string = undefined
+    private lastTab: string | undefined = undefined;
+    private left = 0;
+    private top = 0;
+
+    public constructor(public plugin: Plugin) {
+        super();
+        this.app = this.plugin.app;
+    }
+
+    public override onload(): void {
+        const self = this;
+        this.plugin.addChild(this); // Ensure we unload when hot-reload does
+        this.register(around(Plugin.prototype, {
+            addSettingTab(next) {
+                return function addSettingTabPatched(this: Plugin, tab: PluginSettingTab): void {
+                    next.call(this, tab);
+                    if (!(self.lastTab && this.manifest.id === self.lastTab)) {
+                        return;
+                    }
+                    const { lastTab, left, top } = self;
+                    // Only try this once per plugin id per disable
+                    self.lastTab = undefined;
+                    setTimeout(() => {
+                        if (
+                            self.lastTab // Another state was saved
+                            || !this.app.setting.containerEl.isShown() // Settings not open
+                            || this.app.setting.activeTab // Not on the previously-closed tab
+                        ) {
+                            return;
+                        }
+                        this.app.setting.openTabById(lastTab);
+                        tab.containerEl.scrollTo({ left, top });
+                    }, 100);
+                };
+            }
+        }));
+    }
 
     /**
      * Is the plugin's setting tab active and on-screen?  If so, save its scroll
      * position and set it up to refresh after load.
      */
-    onPluginDisable(pluginID: string) {
-        if (this.app.setting.activeTab?.id === pluginID && this.app.setting.containerEl.isShown()) {
-            const {scrollTop: top, scrollLeft: left} = this.app.setting.activeTab.containerEl;
-            this.lastTab = pluginID;
-            this.left = left;
-            this.top = top;
-            // set up the hook to detect the setting tab registration (if not already set up)
-            this.load();
+    public onPluginDisable(pluginID: string): void {
+        if (!(this.app.setting.activeTab?.id === pluginID && this.app.setting.containerEl.isShown())) {
+            return;
         }
-    }
 
-    onload() {
-        const self = this;
-        this.plugin.addChild(this) // ensure we unload when hot-reload does
-        this.register(around(Plugin.prototype, {
-            addSettingTab(next) {
-                return function(this: Plugin, tab, ...args: any[]) {
-                    next.call(this, tab, ...args);
-                    if (self.lastTab && this.manifest.id === self.lastTab) {
-                        const {lastTab, left, top} = self;
-                        // only try this once per plugin id per disable
-                        self.lastTab = undefined;
-                        setTimeout(() => {
-                            if (
-                                self.lastTab ||  // another state was saved
-                                !this.app.setting.containerEl.isShown() ||  // settings not open
-                                this.app.setting.activeTab  // not on the previously-closed tab
-                            ) return;
-                            this.app.setting.openTabById(lastTab);
-                            tab.containerEl.scrollTo({left, top});
-                        }, 100)
-                    }
-                }
-            }
-        }));
+        const { scrollLeft: left, scrollTop: top } = this.app.setting.activeTab.containerEl;
+        this.lastTab = pluginID;
+        this.left = left;
+        this.top = top;
+        // Set up the hook to detect the setting tab registration (if not already set up)
+        this.load();
     }
 }
 
-declare module "obsidian" {
-    interface Vault {
-        exists(path: string): Promise<boolean>
-        on(type: "raw", handler: (filename: string) => void): EventRef
+function taskQueue(): (action?: () => Promise<void> | void) => Promise<void> {
+    let last: Promise<void> = Promise.resolve();
+    return (action?: () => Promise<void> | void): Promise<void> => {
+        if (action) {
+            last = new Promise<void>((res, rej) => {
+                last.catch(console.error).finally(() => {
+                    try {
+                        res(action());
+                    } catch (e) {
+                        rej(e as Error);
+                    }
+                });
+            });
+        }
+
+        return last;
+    };
+}
+
+export default class HotReload extends Plugin {
+    private asyncRun: (action: () => Promise<void> | void) => Promise<void> = taskQueue();
+    private enabledPlugins = new Set<string>();
+    private pluginNames: Record<string, string> = {};
+    private pluginReloaders: Record<string, () => void> = {};
+
+    private reindexPlugins = debounce(
+        () => {
+            this.run(() => this.getPluginNames());
+        },
+        250,
+        true
+    );
+
+    private settingReloader = new SettingReloader(this);
+    private statCache = new Map<string, Stat>(); // Path -> Stat
+
+    public override onload(): void {
+        this.app.workspace.onLayoutReady(async () => {
+            await this.getPluginNames();
+            this.addCommand({
+                callback: this.reindexPlugins,
+                id: 'scan-for-changes',
+                name: 'Check plugins for changes and reload them'
+            });
+            this.registerEvent(this.app.vault.on('raw', (filename) => {
+                this.onFileChange(filename).catch(console.error);
+            }));
+            await this.watch(this.app.plugins.getPluginFolder());
+        });
     }
-    interface DataAdapter {
-        basePath: string
+
+    private async checkVersion(plugin: string): Promise<void> {
+        const { dir } = this.app.plugins.manifests[plugin] ?? {};
+        if (!dir) {
+            // Deleted plugin, stop watching
+            delete this.pluginNames[plugin];
+            return;
+        }
+
+        for (const file of ['main.js', 'styles.css']) {
+            const path = `${dir}/${file}`;
+            const stat = await this.app.vault.adapter.stat(path);
+            if (stat) {
+                if (this.statCache.has(path) && stat.mtime !== this.statCache.get(path)?.mtime) {
+                    this.requestReload(plugin);
+                }
+                this.statCache.set(path, stat);
+            }
+        }
     }
-    interface FileSystemAdapter extends DataAdapter {
-        watchers: Record<string, unknown>
-        startWatchPath(path: string): void
+
+    private async checkVersions(): Promise<void> {
+        await Promise.all(Object.values(this.pluginNames).map(this.checkVersion.bind(this)));
     }
+
+    private async getPluginNames(): Promise<void> {
+        const enabled = new Set<string>();
+        const plugins: Record<string, string> = {};
+        for (const { dir, id } of Object.values(this.app.plugins.manifests)) {
+            if (!dir) {
+                continue;
+            }
+
+            await this.watch(dir);
+            const pluginName = dir.split('/').pop() ?? '';
+            plugins[pluginName] = id;
+            if (
+                await this.app.vault.exists(`${dir}/.git`)
+                || await this.app.vault.exists(`${dir}/.hotreload`)
+            ) {
+                enabled.add(id);
+            }
+        }
+        this.pluginNames = plugins;
+        this.enabledPlugins = enabled;
+        await this.checkVersions();
+    }
+
+    private isSymlink(path: string): boolean {
+        if (!(this.app.vault.adapter instanceof FileSystemAdapter)) {
+            return false;
+        }
+
+        try {
+            const realPath = [this.app.vault.adapter.basePath, path].join('/');
+            const lstat = this.app.vault.adapter.fs.lstatSync(realPath, { throwIfNoEntry: false });
+            return !!lstat?.isSymbolicLink();
+        } catch {
+            return true;
+        }
+    }
+
+    private async onFileChange(filename: string): Promise<void> {
+        if (!filename.startsWith(`${this.app.plugins.getPluginFolder()}/`)) {
+            return;
+        }
+        const path = filename.split('/');
+        const base = path.pop();
+        const dir = path.pop();
+        if (path.length === 1 && dir === 'plugins') {
+            await this.watch(filename);
+            return;
+        }
+        if (path.length !== 2) {
+            return;
+        }
+        const plugin = dir && this.pluginNames[dir];
+        if (base === 'manifest.json' || base === '.hotreload' || base === '.git' || !plugin) {
+            this.reindexPlugins();
+            return;
+        }
+        if (base !== 'main.js' && base !== 'styles.css') {
+            return;
+        }
+        await this.checkVersion(plugin);
+    }
+
+    private async reload(plugin: string): Promise<void> {
+        const plugins = this.app.plugins;
+
+        // Don't reload disabled plugins
+        if (!plugins.enabledPlugins.has(plugin)) {
+            return;
+        }
+
+        this.settingReloader.onPluginDisable(plugin);
+
+        await plugins.disablePlugin(plugin);
+        console.debug('disabled', plugin);
+
+        // Ensure sourcemaps are loaded (Obsidian 0.14+)
+        const oldDebug = localStorage.getItem('debug-plugin');
+        localStorage.setItem('debug-plugin', '1');
+        const uninstall = preventSourcemapStripping(this.app, plugin);
+        try {
+            await plugins.enablePlugin(plugin);
+        } finally {
+            // Restore previous setting
+            if (oldDebug === null) {
+                localStorage.removeItem('debug-plugin');
+            } else {
+                localStorage.setItem('debug-plugin', oldDebug);
+            }
+            uninstall();
+        }
+        console.debug('enabled', plugin);
+        new Notice(`Plugin "${plugin}" has been reloaded`);
+    }
+
+    private requestReload(plugin: string): void {
+        if (!this.enabledPlugins.has(plugin)) {
+            return;
+        }
+
+        const reloader = this.pluginReloaders[plugin] ?? (
+            this.pluginReloaders[plugin] = debounce(
+                () => {
+                    this.run(() => this.reload(plugin));
+                },
+                750,
+                true
+            )
+        );
+        reloader();
+    }
+
+    private run(action: () => Promise<void> | void): void {
+        this.asyncRun(action).catch(console.error);
+    }
+
+    private async watch(path: string): Promise<void> {
+        if (!(this.app.vault.adapter instanceof FileSystemAdapter)) {
+            return;
+        }
+
+        if (Object.hasOwn(this.app.vault.adapter.watchers, path)) {
+            return;
+        }
+        if ((await this.app.vault.adapter.stat(path))?.type !== 'folder') {
+            return;
+        }
+        if (watchNeeded || this.isSymlink(path)) {
+            this.app.vault.adapter.startWatchPath(path);
+        }
+    }
+}
+
+function preventSourcemapStripping(app: App, pluginName: string): () => void {
+    if (!requireApiVersion('1.6')) {
+        return () => {
+            // No-op
+        };
+    }
+    return around(app.vault.adapter, {
+        read(old) {
+            return async function readPatched(this: DataAdapter, normalizedPath: string) {
+                const txt = await old.call(this, normalizedPath);
+                if (!normalizedPath.endsWith(`/${pluginName}/main.js`)) {
+                    return txt;
+                }
+                return `${txt}\n/* nosourcemap */`;
+            };
+        }
+    });
+}
+
+declare module 'obsidian' {
     interface App {
         plugins: {
-            manifests: Record<string, PluginManifest>
-            getPluginFolder(): string
-            enablePlugin(plugin: string): Promise<void>
-            disablePlugin(plugin: string): Promise<void>
-            enabledPlugins: Set<string>
-            plugins: Record<string, Plugin>
-        }
+            disablePlugin(plugin: string): Promise<void>;
+            enabledPlugins: Set<string>;
+            enablePlugin(plugin: string): Promise<void>;
+            getPluginFolder(): string;
+            manifests: Record<string, PluginManifest>;
+            plugins: Record<string, Plugin>;
+        };
         setting: {
-            activeTab: SettingTab & { id: string } | null
-            containerEl: HTMLElement
-            openTabById(id: string): SettingTab | null
-        }
+            activeTab: { id: string } & SettingTab | null;
+            containerEl: HTMLElement;
+            openTabById(id: string): null | SettingTab;
+        };
+    }
+    interface DataAdapter {
+        basePath: string;
+    }
+    interface FileSystemAdapter extends DataAdapter {
+        fs: typeof import('node:fs');
+        startWatchPath(path: string): void;
+        watchers: Record<string, unknown>;
+    }
+    interface Vault {
+        exists(path: string): Promise<boolean>;
+        on(type: 'raw', handler: (filename: string) => void): EventRef;
     }
 }
